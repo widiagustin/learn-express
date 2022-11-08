904c0b9f40f06f1fd829098683910ca29572db4a
widiagustin <widhianiagustin14@gmail.com>
2022-11-08 21:46:46 +0700

    add db config using sequelize

---
+++ config/sequelize.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize({
+  database: 'cruds',
+  host: 'localhost',
+  username: 'root',
+  password: 'root',
+  dialect: 'mysql'
+})
+
+const koneksi = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+koneksi()
+
+module.exports = sequelize
