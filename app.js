2b8b77a1a5e861379c7f171d7937c710ed2131cb
widiagustin <widhianiagustin14@gmail.com>
2022-11-08 21:47:56 +0700

    add routing V2 for sequelize

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const productRouter = require('./app/product/routes')
+const productRouterV2 = require('./app/product_v2/routes')
 const logger = require('morgan')
 const port = process.env.PORT || 3200
 
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/public', express.static(path.join(__dirname, 'uploads')))
 app.use('/api/v1', productRouter)
+app.use('/api/v2', productRouterV2)
 app.use((req, res, next) => {
   res.status(404)
   res.send({
